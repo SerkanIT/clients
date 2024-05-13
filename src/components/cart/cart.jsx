2fda944090a74fed8c3acc4f551247000962ce18
SerkanIT <jaguarmarka@gmail.com>
2024-05-13 23:07:19 +0500

    serkan

---
+++ src/components/cart/cart.jsx
@@ -0,0 +1,28 @@
+import { totalPrice } from '../../units/total-price';
+import Button from '../button/button';
+import './cart.css';
+
+const Cart = ({ cartItems, onCheckout }) => {
+	return (
+		<div className='cart__container'>
+			<p>
+				Total Price:{' '}
+				{totalPrice(cartItems).toLocaleString('en-US', {
+					style: 'currency',
+					currency: 'USD',
+				})}
+			</p>
+
+			<Button
+				title={`${
+					cartItems.length === 0 ? 'Order' : "Payment"
+				}`}
+				disable={cartItems.length === 0 ? true : false}
+				type={'checkout'}
+				onClick={onCheckout}
+			/>
+		</div>
+	);
+};
+
+export default Cart;
---
+++ src/constants/db.js
@@ -0,0 +1,60 @@
+export function getData() {
+	return [
+		{
+			title: 'Good Looks',
+			price: 10,
+			Image:
+				'https://apps.npr.org/best-books/assets/synced/covers/2023/good-looks.jpg',
+			id: 1,
+		},
+		{
+			title: 'Nutuk',
+			price: 30,
+			Image:
+				'https://cdn.myikas.com/images/4480acec-a8ad-4224-ab1a-558624dc5789/c8c0a693-7793-4f06-83b6-c2488c0ca4f9/image_1080.jpg',
+			id: 2,
+		},
+		{
+			title: 'Kral Şakir',
+			price: 13.5,
+			Image:
+				'https://r.resimlink.com/jP7aoBH.png',
+			id: 3,
+		},
+		{
+			title: 'Harry Potter',
+			price: 13.99,
+			Image:
+				'https://r.resimlink.com/NQc8nBXxW.png',
+			id: 4,
+		},
+		{
+			title: 'Book Thief',
+			price: 12.5,
+			Image:
+				'https://res.cloudinary.com/bookbub/image/upload/t_ci_ar_6:9_padded,f_auto,q_auto,dpr_1/v1659824881/pro_pbid_825589.jpg',
+			id: 5,
+		},
+		{
+			title: 'Good Company',
+			price: 12.99,
+			Image:
+				'https://www.realsimple.com/thmb/SfNg8yAeUTxA7p6OtExOxMjx8-Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Good-Company-Book-Cover-448342647b744db79b6c31c061eef126.jpg',
+			id: 6,
+		},
+		{
+			title: 'After Lives',
+			price: 50,
+			Image:
+				'https://media.newyorker.com/photos/635318ee33e96d379ac69d1a/master/w_1000,c_limit/afterlives.jpg',
+			id: 7,
+		},
+		{
+			title: 'Dead Know',
+			price: 28.99,
+			Image:
+				'https://apps.npr.org/best-books/assets/synced/covers/2023/1982179384.jpg',
+			id: 8,
+		},
+	];
+}
